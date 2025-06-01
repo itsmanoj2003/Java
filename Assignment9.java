3e43b3b817d0820e68f158066002781ae04d1409
Manoj Prabhakaran P <168446711+itsmanoj2003@users.noreply.github.com>
2025-06-01 13:09:57 +0530

    Add files via upload

---
+++ Assignment9.java
@@ -0,0 +1,45 @@
+package Training;
+
+import java.util.*;
+
+public class Assignment9 {
+
+    public static void main(String[] args) {
+
+//        1st Program
+//        int[] arr = {6,0,1,8,0,2};
+//        ArrayList<Integer> zero = new ArrayList<>(5);
+//        ArrayList<Integer> elements = new ArrayList<>(5);
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i] == 0){
+//                zero.add(arr[i]);
+//            }
+//            else {
+//                elements.add(arr[i]);
+//            }
+//        }
+//        elements.addAll(zero);
+//        System.out.println(elements);
+
+
+
+
+        
+//        2nd Program
+//        String str ="aba";
+//        int n =10;
+//        for (int i = 1; i <= n; i++) {
+//            str += str;
+//        }
+//        char[] chararray = str.toCharArray();
+//        int count = 0;
+//        for (int i = 0; i < 10; i++) {
+//            if (chararray[i] == 'a'){
+//                count++;
+//            }
+//        }
+//        System.out.println(count);
+
+    }
+
+}
