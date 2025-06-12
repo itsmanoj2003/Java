c988ee73a71fc25525c88f40f4a8b989c1cbadab
Manoj Prabhakaran P <168446711+itsmanoj2003@users.noreply.github.com>
2025-06-12 07:56:11 +0530

    Add files via upload

---
+++ Assignmentt19.groovy
@@ -0,0 +1,65 @@
+//  1st Program4
+//def input = "Groovy Programming"
+//def vowels = input.toLowerCase().toList().count { it in ['a', 'e', 'i', 'o', 'u'] }
+//println vowels
+
+
+//  2d Program
+//def input = "KnowKode"
+//def reversed = ''
+//for (int i = input.length() - 1; i >= 0; i--) {
+//    reversed += input[i]
+//}
+//println reversed
+
+
+//  3rd Program
+//def isPrime(n) {
+//    if (n <= 1) return false
+//    for (i in 2..<n) {
+//        if (n % i == 0) return false
+//    }
+//    return true
+//}
+//println isPrime(17)
+
+
+//  4th Program
+//def input = [1, 2, 2, 3, 4, 4, 5]
+//def result = input.unique()
+//println result
+
+//    5th Program
+//def list1 = [1, 2, 3, 4]
+//def list2 = [3, 4, 5, 6]
+//def common = list1.intersect(list2)
+//println common
+
+
+
+//  6th Program
+//def areAnagrams(s1, s2) {
+//    s1.toLowerCase().toList().sort() == s2.toLowerCase().toList().sort()
+//}
+//println areAnagrams("listen", "silent")
+
+
+//  7th Program
+//def n = 10
+//def a = 0, b = 1
+//print "$a $b"
+//(2..<n).each {
+//    def sum = a + b
+//    print " $sum"
+//    a = b
+//    b = sum
+//}
+
+
+
+//  8th Program
+//def input = "madam"
+//def isPalindrome = input == input.reverse()
+//println isPalindrome
+
+
